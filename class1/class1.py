f929770f11a4107540ceaeae9ff40af44b58fad9
Mina-39 <haha39938987@gmail.com>
2025-11-20 18:06:33 +0800

    新增 class1.py 檔案，包含基本的打印功能及註解

--- class1/class1.py
+++ class1/class1.py
@@ -1,1 +1,17 @@
-print("hello from class1")
+# 這是單行註解
+# ctrl + ? 可以讓滑鼠框起來的範圍變成註解及快速新增或取消註解
+"""
+這是多行註解1
+這是多行註解2
+這是多行註解3
+"""
+# 基本型態
+print("Hello World")  # 字串 str
+print(123)  # 整數 int
+print(12.34)  # 浮點數 float
+print(True)  # 布林值 True/False bool
+print(None)  # 空值 None
+# 變數命名規則：只能包含字母、數字、底線，且不能以數字開頭，區分大小寫
+hi = "Hello World"  # 把字串 "Hello World" 存到變數 hi 裡面
+# = 的功能是把右邊的東西存到左邊的變數中
+print(hi)  # 印出變數 hi 裡面的東西
